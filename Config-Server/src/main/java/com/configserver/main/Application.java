c032619eb43a0d0af791a0fef6f55cdb9a2f6635
Panneerselvam <Panneer867@live.com>
2023-05-04 10:23:25 +0530

    update

--- Config-Server/src/main/java/com/configserver/main/Application.java
+++ Config-Server/src/main/java/com/configserver/main/Application.java
@@ -2,12 +2,14 @@ package com.configserver.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-public class ConfigServerApplication {
+@EnableConfigServer
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
