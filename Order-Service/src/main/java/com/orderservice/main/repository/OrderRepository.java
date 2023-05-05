92d32842d5e6517ecea0290ccdfc2bf4bce588c5
Panneerselvam <Panneer867@live.com>
2023-05-05 16:21:51 +0530

    update

--- Order-Service/src/main/java/com/orderservice/main/repository/OrderRepository.java
+++ Order-Service/src/main/java/com/orderservice/main/repository/OrderRepository.java
@@ -1,8 +1,11 @@
 package com.orderservice.main.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.orderservice.main.entity.Order;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long>{
 
 }
