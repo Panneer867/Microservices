b3778c4bdcc29e104aae556c95590c2c8e77c4ed
Panneerselvam <Panneer867@live.com>
2023-05-09 14:23:26 +0530

    update

---
+++ Payment-Service/src/test/java/com/paymentservice/main/PaymentServiceTests.java
@@ -0,0 +1,13 @@
+package com.paymentservice.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
